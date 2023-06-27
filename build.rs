afd7e3fac9ba7656327673e6aae5a6bafde147f0
bradhesson <brad.hesson@outlook.com>
2023-06-27 04:20:10 -0600

    cleaned up build.rs

--- build.rs
+++ build.rs
@@ -2,23 +2,20 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to look for shared libraries in the specified directory
-    let header_file = PathBuf::from("lib/NIDAQmx.h");
+    let lib_folder = PathBuf::from("lib").canonicalize().unwrap();
+    let header_file = lib_folder.join("NIDAQmx.h");
 
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rustc-link-search={}", lib_folder.display());
+    println!("cargo:rustc-link-lib=NIDAQmx");
     println!("cargo:rerun-if-changed={}", header_file.display());
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let builder = bindgen::Builder::default()
+    let bindings = bindgen::Builder::default()
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .header(header_file.to_str().unwrap())
-        .detect_include_paths(true);
+        .detect_include_paths(true)
+        .generate()
+        .expect("Unable to generate bindings");
 
-    let bindings = builder.generate().expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
